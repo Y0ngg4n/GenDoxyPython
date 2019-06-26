15fc8533c77528cf7b9829aa9a2bcc38e945a37f
aschuele <andre.schuele@abas-consulting.com>
2019-06-26 16:28:52 +0200

    Fixed Token usage

--- Bitbucket.py
+++ Bitbucket.py
@@ -5,15 +5,15 @@ import os
 def getProjects(baseUrl, token, outputFolder):
     apiUrl = baseUrl + "/rest/api/1.0/"
     print(apiUrl)
-    projects = requests.get(apiUrl + "projects", headers={'Authorization': 'TOK:' + token}).text
+    projects = requests.get(apiUrl + "projects", headers={'Authorization': 'Bearer ' + token}).text
     projects = json.loads(projects)["values"]
     projectKeys = {}
     for project in projects:
         projectKeys[project["key"]] = project["name"]
-        os.makedirs(outputFolder  + "/" + project["name"], exist_ok=True)
+        os.makedirs(outputFolder + "/" + project["name"], exist_ok=True)
 
     for projectKey in projectKeys.keys():
-        repos = requests.get(apiUrl + "projects/" + projectKey + "/repos", headers={'Authorization': 'TOK:' + token}).text
+        repos = requests.get(apiUrl + "projects/" + projectKey + "/repos", headers={'Authorization': 'Bearer ' + token}).text
         repos = json.loads(repos)["values"]
         for repo in repos:
             repoFolder = outputFolder + "/" + projectKeys[projectKey] + "/" + repo["name"]
