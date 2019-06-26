7c15947527a39b5e558aba3306a5c30556b4855e
aschuele <andre.schuele@abas-consulting.com>
2019-06-26 15:07:43 +0200

    Initial Commit

---
+++ Bitbucket.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import os
+
+def getProjects(baseUrl, username, password, outputFolder):
+    apiUrl = baseUrl + "/rest/api/1.0/"
+    print(apiUrl)
+    projects = requests.get(apiUrl + "projects", auth=(username, password)).text
+    projects = json.loads(projects)["values"]
+    projectKeys = {}
+    for project in projects:
+        projectKeys[project["key"]] = project["name"]
+        os.makedirs(outputFolder  + "/" + project["name"], exist_ok=True)
+
+    for projectKey in projectKeys.keys():
+        repos = requests.get(apiUrl + "projects/" + projectKey + "/repos", auth=(username, password)).text
+        repos = json.loads(repos)["values"]
+        for repo in repos:
+            repoFolder = outputFolder + "/" + projectKeys[projectKey] + "/" + repo["name"]
+            os.makedirs(repoFolder, exist_ok=True)
+            httpUrl = ""
+
+            for links in repo["links"]["clone"]:
+                if(links["name"] == "http"):
+                    httpUrl = links["href"]
+
+            os.system("git clone " + httpUrl + " " + repoFolder)
---
+++ Generator.py
@@ -0,0 +1,21 @@
+import sys
+import Bitbucket as bb
+import requests
+
+def getArguments():
+    if(len(sys.argv) < 6):
+        print("Please provide at least 5 Parameter!")
+        return
+    username = sys.argv[2]
+    password = sys.argv[3]
+    baseUrl = sys.argv[4]
+    outputFolder = sys.argv[5]
+
+    if(sys.argv[1]== "bitbucket"):
+        print("Using BitBucket as RMS")
+        bb.getProjects(baseUrl, username, password, outputFolder)
+
+
+
+if __name__ == "__main__":
+    getArguments();
