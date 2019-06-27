90802982330ccec7f7dab4944dbc10cf141040b1
aschuele <andre.schuele@abas-consulting.com>
2019-06-27 16:11:04 +0200

    Added Doxygen

--- Bitbucket.py
+++ Bitbucket.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import os
+import subprocess
 import urllib
+
 
 def getProjects(baseUrl, username, password, outputFolder):
     apiUrl = baseUrl + "/rest/api/1.0/"
@@ -22,9 +24,14 @@ def getProjects(baseUrl, username, password, outputFolder):
             httpUrl = ""
 
             for links in repo["links"]["clone"]:
-                if(links["name"] == "http"):
+                if (links["name"] == "http"):
                     httpUrl = links["href"]
 
             print(httpUrl)
 
             os.system("git clone " + httpUrl + " " + repoFolder)
+            os.system("cp /data/doxygen/Doxyfile Doxyfile.conf")
+            os.system("echo \"INPUT = " + repoFolder + "\" >> Doxyfile.conf")
+            os.system("echo \"OUTPUT_DIRECTORY = " + "/data/doxygen/output/" + projectKeys[projectKey]
+                      + "/" + repo["name"] + "\" >> Doxyfile.conf")
+            os.system("doxygen Doxyfile.conf")
