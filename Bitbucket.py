285507b2bde80237bedcabdef720c4f4e4b45053
aschuele <andre.schuele@abas-consulting.com>
2019-06-27 10:00:15 +0200

    Added netrc

--- Bitbucket.py
+++ Bitbucket.py
@@ -1,22 +1,23 @@
 import requests
 import json
 import os
+import urllib
 
-def getProjects(baseUrl, token, outputFolder):
+def getProjects(baseUrl, username, password, outputFolder):
     apiUrl = baseUrl + "/rest/api/1.0/"
     print(apiUrl)
-    projects = requests.get(apiUrl + "projects", headers={'Authorization': 'Bearer ' + token}).text
+    projects = requests.get(apiUrl + "projects", auth=(username, password)).text
     projects = json.loads(projects)["values"]
     projectKeys = {}
     for project in projects:
         projectKeys[project["key"]] = project["name"]
-        os.makedirs(outputFolder + "/" + project["name"], exist_ok=True)
+        os.makedirs(outputFolder + "/git/" + project["name"], exist_ok=True)
 
     for projectKey in projectKeys.keys():
-        repos = requests.get(apiUrl + "projects/" + projectKey + "/repos", headers={'Authorization': 'Bearer ' + token}).text
+        repos = requests.get(apiUrl + "projects/" + projectKey + "/repos", auth=(username, password)).text
         repos = json.loads(repos)["values"]
         for repo in repos:
-            repoFolder = outputFolder + "/" + projectKeys[projectKey] + "/" + repo["name"]
+            repoFolder = outputFolder + "/git/" + projectKeys[projectKey] + "/" + repo["name"]
             os.makedirs(repoFolder, exist_ok=True)
             httpUrl = ""
 
@@ -24,13 +25,9 @@ def getProjects(baseUrl, token, outputFolder):
                 if(links["name"] == "http"):
                     httpUrl = links["href"]
 
-            if(httpUrl.startswith("http://")):
-                httpUrl = insert_string(httpUrl, token, 6)
-            elif(httpUrl.startswith("https://")):
-                httpUrl = insert_string(httpUrl, token, 7)
+            os.system("echo maschine " + httpUrl.replace("http://", "").replace("https://", "") +  " >> ~/.netrc")
+            os.system("echo login " + username + " >> ~/.netrc")
+            os.system("echo password " + password + " >> ~/.netrc")
             print(httpUrl)
+
             os.system("git clone " + httpUrl + " " + repoFolder)
-
-
-def insert_string(string, index, token):
-    return string[:index] + "x-token-auth:" + token + string[index:]
