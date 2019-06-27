aaad69ec68b4fbd7eee8330396a1bbd007024bae
aschuele <andre.schuele@abas-consulting.com>
2019-06-27 16:46:00 +0200

    Added Branches

--- Bitbucket.py
+++ Bitbucket.py
@@ -30,9 +30,19 @@ def getProjects(baseUrl, username, password, outputFolder):
             print(httpUrl)
 
             os.system("git clone " + httpUrl + " " + repoFolder)
-            os.system("cp /data/doxygen/Doxyfile Doxyfile.conf")
-            os.system("echo \"INPUT = " + repoFolder + "\" >> Doxyfile.conf")
-            os.system("echo \"OUTPUT_DIRECTORY = " + "/data/doxygen/output/" + projectKeys[projectKey]
-                      + "/" + repo["name"] + "\" >> Doxyfile.conf")
-            os.makedirs("/data/doxygen/output/" + projectKeys[projectKey] + "/" + repo["name"], exist_ok=True)
-            os.system("doxygen Doxyfile.conf")
+            outputRepoDir = "/data/doxygen/output/" + projectKeys[projectKey] + "/" + repo["name"]
+            os.makedirs(outputRepoDir, exist_ok=True)
+
+            #Branches
+            subprocess.Popen("cd " + repoFolder + " && git branch", stdout=subprocess.PIPE)
+            outputlines = filter(lambda x: len(x) > 0, (line.strip() for line in subprocess.stdout))
+            for branch in outputlines:
+                outputBranchDir = outputRepoDir + "/" + branch.replace("*", "").trim()
+
+                os.system("echo \"OUTPUT_DIRECTORY = " + outputBranchDir + "\" >> Doxyfile.conf")
+                os.system("cp /data/doxygen/Doxyfile Doxyfile.conf")
+                os.system("echo \"INPUT = " + repoFolder + "\" >> Doxyfile.conf")
+                os.system("echo \"PROJECT_NAME = \"" + repo["name"] + "\"")
+                os.system("cd " + repoFolder + " && git checkout " + branch)
+                os.makedirs(outputBranchDir, exist_ok=True)
+                os.system("doxygen Doxyfile.conf")
