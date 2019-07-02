52ff713483b3cb7fe7fc526d6e804e08dd3cf7b6
aschuele <andre.schuele@abas-consulting.com>
2019-07-02 14:57:46 +0200

    fixed Config

--- Doxygen.py
+++ Doxygen.py
@@ -4,7 +4,7 @@ def generateDocumentation(repoFolder, repoName, outputBranchDir, branch):
     os.system("cp /data/doxygen/Doxyfile Doxyfile.conf")
     os.system("echo \"OUTPUT_DIRECTORY = " + outputBranchDir + "\" >> Doxyfile.conf")
     os.system("echo \"INPUT = " + repoFolder + "\" >> Doxyfile.conf")
-    os.system("echo \"PROJECT_NAME = \"" + repoName + "\" >> Doxyfile.conf")
+    os.system("echo \"PROJECT_NAME = " + repoName + "\" >> Doxyfile.conf")
     print("Switching to " + branch)
     os.system("cd " + repoFolder + " && git checkout " + branch)
     os.makedirs(outputBranchDir, exist_ok=True)
