e4e6e588d5fb7b4e455ac2cc42cd1103472189f4
aschuele <andre.schuele@abas-consulting.com>
2019-07-03 14:34:29 +0200

    Added Github Support

--- Generator.py
+++ Generator.py
@@ -2,6 +2,7 @@
 import sys
 import Bitbucket as bb
 import Gitea as gitea
+import Github as github
 import requests
 import os
 
@@ -26,6 +27,9 @@ def getArguments():
     elif sys.argv[1] == "gitea":
         print("Using Gitea as RMS")
         gitea.getProjects(baseUrl,username, password, outputFolder, genOutPutFolder)
+    elif sys.argv[1] == "github":
+        print("Using Github as RMS")
+        github.getProjects(baseUrl,username, password, outputFolder, genOutPutFolder)
 
 if __name__ == "__main__":
     getArguments()
---
+++ Github.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import os
+import subprocess
+import re
+import Doxygen
+
+
+def getProjects(baseUrl, username, password, outputFolder, genOutPutFolder):
+    apiUrl = baseUrl.replace("http://", "http://api."+baseUrl).replace("https://api." + baseUrl)
+
+    repos = requests.get(apiUrl + "user/subscriptions", auth=(username, password)).text.encode("ascii",
+                                                                                               "ignore").decode("ascii")
+    repos = json.loads(repos)
+    print(repos)
+    for repo in repos:
+        print(repo)
+        repoFolder = outputFolder + "/git/" + repo["owner"]["login"] + "/" + repo["name"]
+        os.makedirs(repoFolder, exist_ok=True)
+        httpUrl = repo["clone_url"]
+        os.system("git clone " + httpUrl + " " + repoFolder)
+        outputRepoDir = genOutPutFolder + "/" + repo["owner"]["login"] + "/" + repo["name"]
+        os.makedirs(outputRepoDir, exist_ok=True)
+
+        # Branches
+        proc = subprocess.Popen("cd " + repoFolder + " && git branch -a", stdout=subprocess.PIPE, shell=True)
+        lines = proc.stdout.readlines();
+        for branch in lines:
+            branch = re.sub(".*\/", "", branch.decode("ascii").replace("*", "").replace("\r", "").strip())
+            outputBranchDir = outputRepoDir + "/" + branch
+            Doxygen.generateDocumentation(repoFolder, repo["name"], outputBranchDir, branch)
